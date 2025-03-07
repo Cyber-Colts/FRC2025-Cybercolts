07618c17fad6f7058cce7d558c39a1058602231e
lusyi <115723652+lusyi@users.noreply.github.com>
2025-03-07 09:28:45 -0500

    Add files via upload
    Additions: 
    LimelightHelpers v1.11 - java file
    src robot limelight - basic targeting data and distance estimation using april tags (84 - 102) -- proportional control loop, drives to set distance from the goal, doesn't work yet (201 - 210)
    autonomous - time based autonomous drive (178 - 198)
    imported libraries - somewhere in there, some are currently unused lol
    
    comments: the time based autonomous drive uses the macanum drive WPI template so we might have to change it to adapt to the REV lib templates, and the arm movement hasn't been implemented to autonomous yet.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,64 +15,94 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.EncoderConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.studica.frc.AHRS;
 import com.studica.frc.AHRS.NavXComType;
 
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
-import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import pabeles.concurrency.ConcurrencyOps.Reset;
+import edu.wpi.first.apriltag.AprilTagDetection;
+import edu.wpi.first.apriltag.jni.AprilTagJNI;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Timer;
+
+
 
 public class Robot extends TimedRobot {
-        
   private final CANBus kCANBus = new CANBus();
-        
+
   private final TalonFX leftLeader = new TalonFX(1, kCANBus);
   private final TalonFX leftFollower = new TalonFX(2, kCANBus);
   private final TalonFX rightLeader = new TalonFX(3, kCANBus);
   private final TalonFX rightFollower = new TalonFX(4, kCANBus);
-          
+  
   private final DutyCycleOut leftOut = new DutyCycleOut(0);
   private final DutyCycleOut rightOut = new DutyCycleOut(0);
+
+  private MecanumDrive m_robotDrive;
+  
+  private final Timer m_timer = new Timer();
+
   SparkMax turntable;
   SparkMax armPitch;
   SparkMax armPitch2;
   SparkMax armExt;
-  SparkMax Rollers;
-  SparkMax IntakeExt;
-          
-  double encoderRaw;
-  double encoderRawL;
-  double encoderRawR;
-  double wheelPosL;
-  double wheelPosR;
-        
-        
-  PS4Controller joystick;
-  Joystick joystick1;
-  DifferentialDriveOdometry m_odometry;
-  // Creating my kinematics object: track width of 27 inches
-  DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(0.58);
-  // Example chassis speeds: 2 meters per second linear velocity,
-  // 1 radian per second angular velocity.
-  ChassisSpeeds chassisSpeeds = new ChassisSpeeds(2.0, 0, 1.0);
-  // Convert to wheel speeds
-  DifferentialDriveWheelSpeeds wheelSpeeds;
+  SparkMax hand;
+  
+  double encoderRaw; double encoderRawL; double encoderRawR;
+  double wheelPosL; double wheelPosR;
+  double avgTagArea; double avgTagDist;
+  double x; double y; 
+  double area; 
+  double left_command; double right_command; double forward_command; double backward_command; 
+  double xHeading_error; double yHeading_error; 
+  double x_adjust; double y_adjust;
 
-  // Left velocity
-  double leftVelocity = wheelSpeeds.leftMetersPerSecond;
-  // Right velocity
-  double rightVelocity = wheelSpeeds.rightMetersPerSecond;
+  float Kp;
+  float min_command;
+
+  // Basic targeting data
+  double tx = LimelightHelpers.getTX("");  // Horizontal offset from crosshair to target in degrees
+  double ty = LimelightHelpers.getTY("");  // Vertical offset from crosshair to target in degrees
+  double ta = LimelightHelpers.getTA("");  // Target area (0% to 100% of image)
+  boolean hasTarget = LimelightHelpers.getTV(""); // Do you have a valid target?
+
+  double txnc = LimelightHelpers.getTXNC("");  // Horizontal offset from principal pixel/point to target in degrees
+  double tync = LimelightHelpers.getTYNC("");  // Vertical  offset from principal pixel/point to target in degrees
+
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tableE = table.getEntry("tableE");
+  double targetOffsetAngle_Vertical = tableE.getDouble(0.0);
+  double limelightMountAngleDegrees = 25.0; // how many degrees back is your limelight rotated from perfectly vertical?
+  double limelightLensHeightInches = 20.0; // distance from the center of the Limelight lens to the floor
+  double goalHeightInches = 60.0; // distance from the target to the floor
+  double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+  double angleToGoalRadians = angleToGoalDegrees * (3.14159/180.0); // converts to radians
+  
+  double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians); //calculate distance
+
+  GenericHID joystick;
+  PS4Controller joystick1;
   
   AHRS m_gyro = new AHRS(NavXComType.kMXP_SPI);
   
@@ -85,17 +115,18 @@ public class Robot extends TimedRobot {
   Field2d m_field = new Field2d();
 
 
+
   public Robot() {
-    
     var leftConfiguration = new TalonFXConfiguration();
     var rightConfiguration = new TalonFXConfiguration();
     turntable = new SparkMax(5, MotorType.kBrushless);
     armPitch = new SparkMax(6, MotorType.kBrushless);
     armPitch2 = new SparkMax(7, MotorType.kBrushless);
-    armExt = new SparkMax(8, MotorType.kBrushless);
+
 
     SparkMaxConfig launcherConfig = new SparkMaxConfig();
 
+    //table = NetworkTable.getTable("datatable");
 
     /* User can optionally change the configs or leave it alone to perform a factory default */
     leftConfiguration.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
@@ -110,7 +141,6 @@ public class Robot extends TimedRobot {
     turntable.configure(launcherConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     armPitch.configure(launcherConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     armPitch2.configure(launcherConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    armExt.configure(launcherConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
     leftLeader.getConfigurator().apply(leftConfiguration);
     leftFollower.getConfigurator().apply(leftConfiguration);
@@ -119,25 +149,14 @@ public class Robot extends TimedRobot {
 
     leftLeader.setSafetyEnabled(true);
     rightLeader.setSafetyEnabled(true);
-    // Initialize joystick
-    joystick = new PS4Controller(0);
-    joystick1 = new Joystick(1);
 
-    m_odometry = new DifferentialDriveOdometry(
-      m_gyro.getRotation2d(),
-      getEncoderLeft(), getEncoderRight(),
-      new Pose2d(5.0, 13.5, new Rotation2d()));
+    // Initialize joystick
+    joystick = new Joystick(0);
+    joystick1 = new PS4Controller(1);
   }
 
   @Override
   public void robotPeriodic() {
-    var wheelSpeeds = kinematics.toWheelSpeeds(chassisSpeeds);
-    //Update Pose on field
-    m_odometry.update(m_gyro.getRotation2d(),
-      getEncoderLeft(),
-      getEncoderRight());
-     
-    m_field.setRobotPose(m_odometry.getPoseMeters());
     // Display the applied output of the left and right side onto the dashboard
     SmartDashboard.putData("Field", m_field);
     SmartDashboard.putBoolean("Teleop Is On", isTeleopEnabled());
@@ -146,23 +165,89 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("Left Encoder", getEncoderLeft());
     SmartDashboard.putNumber("Turntable Encoder", getEncoderTurntable());
     SmartDashboard.putNumber("Speed", m_gyro.getRobotCentricVelocityX());
-    SmartDashboard.putNumber("ODOMVelocityL", leftVelocity);
-    SmartDashboard.putNumber("ODOMVelocityR", rightVelocity);
     SmartDashboard.putNumber("Pitch", m_gyro.getPitch());
+    DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(
+      m_gyro.getRotation2d(),
+      getEncoderLeft(), getEncoderRight(),
+      new Pose2d(5.0, 13.5, new Rotation2d()));
+      m_field.getRobotPose();
+      m_field.setRobotPose(m_odometry.getPoseMeters());
     SmartDashboard.putData("PDP",PDP);
   }
 
   @Override
   public void autonomousInit() {
+    m_timer.reset();
+    m_timer.start();
   }
 
   @Override
   public void autonomousPeriodic() {
+    
+    double heading = m_gyro.getAngle();
+    if (m_timer.get() > 0.0 && m_timer.get() < 2.0) { //DRIVE FORWARD, 2 SECONDS
+      m_robotDrive.driveCartesian(.5, 0, 0);
+    }
+    else if (m_timer.get() > 2.0 && m_timer.get() < 10.0) { //BETWEEN 2 AND 10 SECONDS, MOVE TO FACE 90 DEGREES
+        if (heading < 89.0) {
+          m_robotDrive.driveCartesian(0, 0, .4);
+        }
+        else if (heading > 91.0) {
+          m_robotDrive.driveCartesian(0, 0, -.4);
+        }
+    }
+    
+    /* 
+    float KpDistance = -0.1f;  // Proportional control constant for distance
+    float current_distance = distanceFromLimelightToGoalInches();  // see the 'Case Study: Estimating Distance' 
+
+    if (m_timer.get() > 2.0) {
+      float distance_error = desired_distance - current_distance;
+      driving_adjust = KpDistance * distance_error;
+    
+      left_command += distance_adjust;
+      right_command += distance_adjust;
+    }
+    */
   }
+
 
   @Override
   public void teleopInit() {
   }
+
+  public void limelightTarget() {
+    xHeading_error = -x;
+    yHeading_error = -y;
+    x_adjust = 0.0f;
+    y_adjust = 0.0f;
+    if (x > 1.0)
+    {
+            x_adjust = Kp*xHeading_error - min_command;
+    }
+    else if (x < 1.0)
+    {
+            x_adjust = Kp*xHeading_error + min_command;
+    }
+    left_command += x_adjust;
+    right_command -= x_adjust;
+
+    if (y > 1.0)
+    {
+      y_adjust = Kp*yHeading_error - min_command;
+    }
+    else if (y < 1.0)
+    {
+      y_adjust = Kp*yHeading_error + min_command;
+    }
+    forward_command += y_adjust;
+    backward_command -= y_adjust;
+
+    m_robotDrive.driveCartesian(y_adjust, x_adjust, 0.0);
+
+    //https://docs.limelightvision.io/en/latest/cs_estimating_distance.html
+  }
+
   //TELEOP CODE
   @Override
   public void teleopPeriodic() {
@@ -172,50 +257,52 @@ public class Robot extends TimedRobot {
      */
     /* Get forward and rotational throttle from joystick */
     /* invert the joystick Y because forward Y is negative */
-    double fwd = -joystick.getRawAxis(1);
-    double rot = joystick.getRawAxis(2);
-    double multiplier = 0.6;
-    double multiplierNormal = 0.3;
-    double multiplierTable = 0.3; 
-    double multiplierSLOW = 0.1;
+    double fwd = -joystick1.getRawAxis(1);
+    double rot = joystick1.getRawAxis(2);
+    //if(fwd != 0){
+    /* Set up followers to follow leaders */
+    //leftFollower.setControl(new Follower(leftLeader.getDeviceID(), true));
+    //rightFollower.setControl(new Follower(rightLeader.getDeviceID(), true));
+    //}
+    //if(rot != 0){
+    /* Set up followers to follow leaders */
+    //leftFollower.setControl(new Follower(leftLeader.getDeviceID(), false));
+    //rightFollower.setControl(new Follower(rightLeader.getDeviceID(), false));    
+    //}
 
-    // Apply the multiplier to the joystick inputs
-    if (joystick.getR1Button()) {
-      rot *= multiplier;
-      fwd *= multiplier;
+    if(joystick.getRawButton(5)){
+      //set limit for turntable
+      if(360 / 4 * turntable.getAbsoluteEncoder().getPosition() > -360) {
+        turntable.set(-20);
+      }
+      else {
+        turntable.set(0);
+      }
     }
-    else{
-      rot *= multiplierNormal;
-      fwd *= multiplierNormal;
+    if(!joystick.getRawButton(5)){
+      turntable.set(0);
     }
-    double pitchturn = joystick1.getRawAxis(1);
-    double rotturn = -joystick1.getRawAxis(0);
-    pitchturn *= multiplierSLOW;
-    rotturn *= multiplierTable;
-    if(joystick.getPOV() >= 270 && joystick.getPOV() <= 90){
-      rot *= multiplierSLOW;
-      fwd *= multiplierSLOW;
-      leftOut.Output = 1;
-      rightOut.Output = -1;
+    if(joystick.getRawButton(6)){
+      //set limit for turntable
+      if(360 / 4 * turntable.getAbsoluteEncoder().getPosition() > 360) {
+        turntable.set(20);
+      }
+      else {
+        turntable.set(0);
+      }
     }
-    else if (joystick.getPOV() <= 270 && joystick.getPOV() >= 90)
-    {
-      rot *= multiplierSLOW;
-      fwd *= multiplierSLOW;
-      leftOut.Output = -1;
-      rightOut.Output = 1;
+    if(!joystick.getRawButton(6)){
+      turntable.set(0);
     }
+
+
     /* Set output to control frames */
     leftOut.Output = fwd + rot;
     rightOut.Output = fwd - rot;
     /* And set them to the motors */
     if (!joystick1.getRawButton(1)) {
       leftLeader.setControl(leftOut);
-      //set pid values
       rightLeader.setControl(rightOut);
-      armPitch.set(pitchturn);
-      armPitch2.set(-pitchturn);
-      turntable.set(rotturn);
     }
   }
     
@@ -230,9 +317,6 @@ public class Robot extends TimedRobot {
      rightOut.Output = 0;
      leftLeader.setControl(leftOut);
      rightLeader.setControl(rightOut);
-     armPitch.set(0);
-     armPitch2.set(0);
-     turntable.set(0);
   }
 
   @Override
